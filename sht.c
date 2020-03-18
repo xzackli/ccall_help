b655d3527cbc015c3561620d44aff88ee4443336
xzackli <xzackli@gmail.com>
2020-03-18 12:39:23 -0400

    works thanks to instructions from yuyichao

--- sht.c
+++ sht.c
@@ -5,7 +5,7 @@ int sht(double** origin, dcmplx** target,
     // copy origin elements to target for each component
     for (int i=0;i<(int)(num_components);++i) {
         for (int j=0;j<(int)(array_size);++j) {
-            (target[i])[j] = (origin[i])[j];
+            (target[i])[j] = (origin[i])[j] + 0.0*_Complex_I;
         }
     }
     return 1;
--- sht.jl
+++ sht.jl
@@ -1,15 +1,12 @@
-#  set up 
-origin = [[Cdouble(i) for i in 1:3]]
-target = [zeros(ComplexF64, 3)]
-
-pointer_origin = pointer(origin,1)
-pointer_target = pointer(target,1)
+# set up 
+origin = [Cdouble(i) for i in 1:3]
+target = zeros(ComplexF64, 3)
 
 # perform the call
-ccall(
+GC.@preserve origin target ccall(
     (:sht, "./sht.so"),
     Cint,
     (Ptr{Ptr{Cdouble}}, Ptr{Ptr{ComplexF64}}, Cint, Cint),
-    pointer_origin, pointer_target, 3, 1)
+    [pointer(origin)], [pointer(target)], 3, 1)
 
 println(target)  # want this to be [1.0, 2.0, 3.0]
